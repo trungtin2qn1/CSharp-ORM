5db400f6da03ec737001dcc1a5f37afcc0e31099
Tin Huynh <trungtin2qn1@gmail.com>
2019-12-30 22:36:14 -0800

    Inititalize Repository

---
+++ SEP_Framework/SEP_Framework/Utils/Crypto.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEP_Framework.Utils
+{
+    public class Crypto
+    {
+        public static string hash = "foxle@rn";
+        public static string Encrypt(string value)
+        {
+            byte[] data = UTF8Encoding.UTF8.GetBytes(value);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider triple = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = triple.CreateEncryptor();
+                    byte[] res = transform.TransformFinalBlock(data, 0, data.Length);
+                    return Convert.ToBase64String(res, 0, res.Length);
+                }
+            }
+        }
+        public static string Decrypt(string value)
+        {
+            byte[] data = Convert.FromBase64String(value);
+            using (MD5CryptoServiceProvider md5 = new MD5CryptoServiceProvider())
+            {
+                byte[] keys = md5.ComputeHash(UTF8Encoding.UTF8.GetBytes(hash));
+                using (TripleDESCryptoServiceProvider triple = new TripleDESCryptoServiceProvider() { Key = keys, Mode = CipherMode.ECB, Padding = PaddingMode.PKCS7 })
+                {
+                    ICryptoTransform transform = triple.CreateDecryptor();
+                    byte[] res = transform.TransformFinalBlock(data, 0, data.Length);
+                    return UTF8Encoding.UTF8.GetString(res);
+                }
+            }
+        }
+    }
+}
---
+++ SEP_Framework/SEP_Framework/Utils/Router.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SEP_Framework.Utils
+{
+    public class Router
+    {
+        private static Dictionary<string, bool> isShow = new Dictionary<string, bool>();
+
+        public static void ChangeForm(Form currentForm, Form nextForm, bool turnBackAble = false)
+        {
+            nextForm.Location = currentForm.Location;
+            nextForm.StartPosition = FormStartPosition.Manual;
+            if (turnBackAble)
+            {
+                nextForm.FormClosing += delegate { currentForm.Show(); };
+            }
+            else
+            {
+                nextForm.FormClosing += delegate { currentForm.Close(); };
+            }
+            nextForm.Show();
+            currentForm.Hide();
+        }
+
+        public static void ShowFormDialog(Form currentForm, Form frm)
+        {
+            string frmName = frm.Text;
+
+            if (isShow.ContainsKey(frmName) && isShow[frmName])
+            {
+                MessageBox.Show("Form này đang được mở", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+            else
+            {
+                frm.Location = currentForm.Location;
+                frm.StartPosition = FormStartPosition.CenterScreen;
+                isShow[frmName] = true;
+                frm.FormClosed += (o, e) => isShow[frmName] = false;
+                frm.Show();
+            }
+        }
+    }
+}

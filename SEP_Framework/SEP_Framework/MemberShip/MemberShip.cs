5cbcd272313120aede75c40d5db943ead1b95e61
Tri <lhhtri2811@gmail.com>
2019-12-31 17:20:57 +0700

    add class MemberShip

---
+++ SEP_Framework/SEP_Framework/MemberShip/MemberShip.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEP_Framework.MemberShip
+{
+    public class Member
+    {
+        private HandleController controller;
+
+        public Member(string cnnString)
+        {
+            controller = new HandleController(cnnString);
+            controller.createSessionTable();
+        }
+
+        public bool Login(string username, string password)
+        {
+            if (controller.Login(username, password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Register(string username, string password)
+        {
+            if (controller.Register(username, password))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Logout(string username)
+        {
+            if (controller.Logout(username))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
